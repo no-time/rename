39dfd3c5339f61facb78307296bce47868fcbfd5
no-time <cdedbd_wings@hotmail.com>
2014-06-18 14:33:36 -0500

    Added some modifications/improvements
    Improved single file rename to have directory first and added '/' character to end of directory names.

--- renamer.py
+++ renamer.py
@@ -1,12 +1,18 @@
 def main():
     while True:
         question1=input('''
+(Enter 'q' to quit)
+
 Do you want to rename:
 
 1. A single file
 2. Multiple files?
 
 (1, or 2):''')
+
+        if question1 == 'q':
+            break
+            
         if question1 == '1':
         
             single("1")
@@ -23,7 +29,7 @@ def single(multi):
     import os
     import subprocess
     from subprocess import call
-    proc = subprocess.Popen(["ls", "-a"], \
+    proc = subprocess.Popen(["ls --group-directories-first -p"], \
     stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     ls=str(out)
@@ -33,22 +39,37 @@ def single(multi):
     y=str(y[:-1])
     ls_split[0]=y
     ls_split.pop()
-    print(ls_split)
+    #print(ls_split)
     number=0
     for filenumber in ls_split:
-        print(number, ls_split[number])
+        print(str(number)+'.', ls_split[number])
         number+=1
     if multi== "1":
         single2(ls_split)
+    
 def single2(ls_split):
     import os
     import subprocess
     from subprocess import call
-    user_input=int(input\
-    ("Which file number would you like to rename?: "))
-    rename= input("What would you like to call the file?: ")
-    
-    call(["mv","-i",str(ls_split[user_input]),str(rename)])
+    while True:
+        user_input=int(input\
+    ("Which number would you like to rename? : "))
+        if int(user_input) <= (int(len(ls_split))-1):
+            break
+    rename= input("What would you like to call the file? (press 'q' \
+to quit): ")
+    if rename == 'q':
+        os.sys.exit()
+    extens= input("What file extension? (Type everything after '.'): ")
+    if extens =="":
+        rename= rename+str(extens)
+    if extens !="":
+        rename= rename+'.'+str(extens)
+    confirm = input("\nAre you sure you want to rename: '"+ \
+                    str(ls_split[user_input])+"' with '" +str(rename)\
+                    + "'?\n(Press 'y' to confirm): ")
+    if confirm == 'y':
+        call(["mv","-i",str(ls_split[user_input]),str(rename)])
 
 def multi():
     import os
@@ -60,14 +81,16 @@ def multi():
     (out, err) = proc.communicate()
     directory=str(out[1:-1])
     directory="/"+directory[2:-1]+"/"
-    print(directory)
+    print('Current Directory:\n'+(str)(directory))
     integer=0
     extension=input("\nEnter the file extension: ")
     fileList=multi1(extension)
     print(fileList)
     user_inputM=input("\nEnter the beginning part of the filename\
 . It will be numbered with this beginning \
-(i.e. name0, name1, name2): ")
+(i.e. name0, name1, name2)\n(Or type 'q' to exit):\n")
+    if user_inputM == 'q':
+        os.sys.exit()
     for file in fileList:
         call(["mv","-i",str(fileList[(integer)]),str(user_inputM)+\
               str(integer)+str(extension)])
